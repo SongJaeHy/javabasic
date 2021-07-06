9adffc26d51c976456433eae30bac5b3f077032c
SongJaeHy <rnswothd@naver.com>
2021-07-06 15:12:19 +0900

    추가함

---
+++ 02-javabasic/src/Case1.java
@@ -0,0 +1,19 @@
+
+public class Case1 {
+	public static void main(String[] args) {
+		int a = 3;
+		switch(a) {
+		case 1:
+			System.out.println("1");
+		case 2:
+			System.out.println("1");
+		case 3:
+			System.out.println("1");
+		case 4:
+			System.out.println("1");
+		break;
+		case 5:
+			System.out.println("1");
+		}
+	}
+}
