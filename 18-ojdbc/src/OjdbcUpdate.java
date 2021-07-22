f9d85f380bd43bf7c3a4a634b8f6c50e538d2818
SongJaeHy <rnswothd@naver.com>
2021-07-22 16:31:37 +0900

    22

--- 18-ojdbc/src/OjdbcUpdate.java
+++ 18-ojdbc/src/OjdbcUpdate.java
@@ -1,20 +1,9 @@
 import java.sql.*;
-import java.util.Scanner;
-public class OjdbcInsert {
+public class OjdbcUpdate {
 
 	public static void main(String[] args) {
 		Connection con = null;
-		// 기존 Statement는 SQL구문과 자바 구문의 경계가 불분명했습니다.
-		// 그래서 자바코드와 SQL구문이 섞여서 굉장히 지저분했는데
-		// PreparedStatment는 SQL구문만 먼저 작성해두고
-		// 필요한 부분에만 자바 변수를 대입하는 식으로 작성해 가독성, 작성용이성이
-		// 훨씬 좋아졌고, 사실상 표준처럼 사용합니다.
 		PreparedStatement pstmt = null;
-		Scanner scan = new Scanner(System.in);
-		System.out.println("정수를 입력해주세요.");
-		int snum = scan.nextInt();
-		System.out.println("문자열를 입력해주세요.");
-		String nstr = scan.next();
 		
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
@@ -27,16 +16,16 @@ public class OjdbcInsert {
 			// 1.PreparedStatement 구문은 쿼리문을 먼저 만들고 시작합니다.
 			// 기존의 자바 변수가 들어갈 자리에 ? 로 대신해서 작성합니다.
 			// 자료형 막론하고 문자열에도 ' 를 붙이지 않고 ?로만 처리하면 됩니다.
-			String sql = "INSERT INTO ojdbctest(num, str) VALUES "
-					+ "(?,?)";
+			String sql = "UPDATE ojdbctest set str="+ "?" + "WHERE num="+"?";
+					
 			
 			// 2. 만든 쿼리문의 ? 자리에 적용할 자바 변수를 집어넣습니다.
 			// pstmt변수를 생성해놓은 다음, set자료형(?순서, 집어넣을 변수)
 			// 를 이용해 ?를 모두 채워줍니다.
 			// 아래 코드는 첫 번째 ?에는 1을, 두 번째 ?에는 "test"를 대신 넣겠다는 의미
 			pstmt = con.prepareStatement(sql);
-			pstmt.setInt(1, snum);
-			pstmt.setString(2, nstr);
+			pstmt.setInt(1, 1);
+			pstmt.setString(2, "?");
 			
 			
 			// 3. 입력이 모두 끝난 뭐리문을 실행합니다.
@@ -55,7 +44,6 @@ public class OjdbcInsert {
 				e.printStackTrace();
 			}
 		}
-
 	}
 
 }
