a16cfb791af5cfd049b03fbebc583813106cc49c
SongJaeHy <rnswothd@naver.com>
2021-07-22 18:00:56 +0900

    최종

--- 18-ojdbc/src/OjdbcDelete.java
+++ 18-ojdbc/src/OjdbcDelete.java
@@ -1,29 +1,27 @@
 import java.sql.*;
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class OjdbcDelete {
 
 	public static void main(String[] args) {
 		Connection con = null;
 		PreparedStatement pstmt = null;
-		Scanner scan = new Scanner(System.in);
-		
-		System.out.println("숫자를 입력해주세요.");
-		int Vnum = scan.nextInt();
+//		Scanner scan = new Scanner(System.in);
+//		
+//		System.out.println("숫자를 입력해주세요.");
+//		int Vnum = scan.nextInt();
 		
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			String url = "jdbc:oracle:thin:@localhost:1521/XEPDB1";
 			con = DriverManager.getConnection(url, "mytest", "mytest");
 			
-			String sql = "DELETE FROM ojdbctest WHERE num=?";
+			String sql = "DELETE FROM ojdbctest";
 			
 			pstmt = con.prepareStatement(sql);
-			pstmt.setInt(1, Vnum);
-			
 			
 			pstmt.executeUpdate();
-			
+		
 		} catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패");
 		} catch (SQLException e) {
@@ -35,9 +33,10 @@ public class OjdbcDelete {
 				}
 			}catch(SQLException e) {
 				e.printStackTrace();
+				
 			}
+		
 		}
-
 	}
 
 }
